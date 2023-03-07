a2c36ecf6ff1c422a9d98ebfd6451be887ba6806
thomaskaufi <thomas@kaufmanas.dk>
2023-03-07 14:30:27 +0100

    include

--- arduino examples/LEDstrip_Barebone/LEDstrip_Barebone.ino
+++ arduino examples/LEDstrip_Barebone/LEDstrip_Barebone.ino
@@ -1,15 +1,21 @@
-#include <FastLED.h>
+/* LEDstrip Barebone | AIRLAB CCC#2
+ * 
+ * The code serves as an absolut minimum for working with LED strips
+ * 
+ * Syntax for changing color:
+ * leds[number of LED] = CRGB(Red value, Green value, Blue value); (Values are 0-255)
+ * FastLED.show()
+ * 
+ */
 
-#define NUM_LEDS 1
-#define DATA_PIN A5
-// Define the array of leds
+#include <FastLED.h>
+#define NUM_LEDS 10
+#define DATA_PIN 3
 CRGB leds[NUM_LEDS];
+
 
 void setup() 
 {
-  //initialize serial communications at a 9600 baud rate
-  Serial.begin(9600);
-
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   
   // turn off the LED
@@ -17,21 +23,17 @@ void setup()
   FastLED.show();
 }
 
+
 void loop()
 {
-  // turn on the LED (red color)
-  leds[0] = CRGB(255,0,0);
+  // turn on LED [3] (the fourth)
+  leds[3] = CRGB(255,80,190);
   FastLED.show();
-  
   delay(1000); // wait 1 sec
   
-  // turn off the LED
-  leds[0] = CRGB(0,0,0);
+  // turn off LED [3] (the fourth)
+  leds[3] = CRGB(0,0,0);
   FastLED.show();
-  
   delay(1000); // wait 1 sec
-  
-
-
 
 }
