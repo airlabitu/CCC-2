153b5c5cbe1ff3b8e2e1671630dcbdbb7023c7b3
thomaskaufi <thomas@kaufmanas.dk>
2023-03-07 12:45:09 +0100

    Kammertonen:)

--- arduino examples/tone_barebone/tone_barebone.ino
+++ arduino examples/tone_barebone/tone_barebone.ino
@@ -3,7 +3,7 @@ int soundPin = 8;
 void setup() {
   // put your setup code here, to run once:
   pinMode(soundPin, OUTPUT);
-  tone(soundPin,  300); // setting one frequency 'once'
+  tone(soundPin,  440); // setting one frequency 'once'
 }
 
 void loop() {
